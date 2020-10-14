d091345efe5d114c832e9a729235199e231cb287
周寒 <5668235+zhou_han520@user.noreply.gitee.com>
2020-10-14 10:06:38 +0800

    first commit

---
+++ src/app2.js
@@ -0,0 +1,16 @@
+import $ from 'jquery';
+import './app2.css'
+const $tabBar=$('#app2 .tab-bar');
+const $tabContent=$('#app2 .tab-content')
+$tabBar.on('click','li',e=>{
+  const $li=$(e.currentTarget);
+  $li.addClass('selected')
+  .siblings()
+  .removeClass('selected')
+  const index=$li.index()
+  console.log(index);
+  $tabContent.children()
+  .eq(index).addClass('active')
+  .siblings().removeClass('active')  // 兄弟变成隐藏 
+})
+$tabBar.children().eq(0).trigger('click')
---
+++ src/app3.js
@@ -0,0 +1,6 @@
+import $ from 'jquery'
+import './app3.css'
+const $square=$('#app3 .square')
+$square.on('click',()=>{
+    $square.toggleClass('active')
+})
